e8ebcb84092872301986be4a7efb0a6a9fc67f04
Izzy Gibson <izzyyg27@gmail.com>
2022-08-15 13:49:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from colorama import init, Fore
 import os
 import time
+import random
+import requests
 
-APPLICATIONS = [
-    'https://updates.insomnia.rest/downloads/windows/latest?app=com.insomnia.app&source=website']
+AUTORUN_APPS = [
+    'https://updates.insomnia.rest/downloads/windows/latest?app=com.insomnia.app&source=website',
+    'https://download.scdn.co/SpotifySetup.exe',
+    'https://www.google.com/chrome/thank-you.html?statcb=0&installdataindex=empty&defaultbrowser=0#'
+]
+
+MANUALLY_INSTALLED_APPS = [
+    'https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user',
+    'https://github.com/git-for-windows/git/releases/download/v2.37.2.windows.2/Git-2.37.2.2-64-bit.exe',
+    'https://www.dropbox.com/s/rmc4kb6b5zj7myk/OfficeSetup.exe?dl=1',
+    'https://central.github.com/deployments/desktop/desktop/latest/win32',
+    'https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-611.exe',
+    'https://www.python.org/ftp/python/3.8.10/python-3.8.10-amd64.exe',
+    'https://github.com/canton7/SyncTrayzor/releases/download/v1.1.29/SyncTrayzorSetup-x64.exe',
+    'https://api2.prod.symless.com/aws-downloads/synergy/v1-core-standard/1.14.5-stable.a975f61a/synergy_1.14.5-stable.a975f61a_windows_x64.msi',
+    'https://mirror.fcix.net/videolan-ftp/vlc/3.0.17.4/win64/vlc-3.0.17.4-win64.exe'
+]
 
 # Get system information
 os.system('cls')
@@ -25,3 +42,21 @@ for line in sysinfo:
           (' ' * (60 - (len(a) - 4) - len(b))) + b + (' ' * 20))
     print(ln)
     time.sleep(0.2)
+
+if not os.path.exists('single_click'):
+    os.makedirs('single_click')
+
+if not os.path.exists('manual_setup'):
+    os.makedirs('manual_setup')
+
+for a in AUTORUN_APPS:
+    print(f'{Fore.LIGHTGREEN_EX}{a}{Fore.RESET}')
+    path = f'single_click/{str(random.randint(11111,99999))}.exe'
+    open(path, 'wb').write(requests.get(a).content)
+
+time.sleep(10)
+
+for a in MANUALLY_INSTALLED_APPS:
+    print(f'{Fore.LIGHTGREEN_EX}{a}{Fore.RESET}')
+    path = f'manual_setup/{str(random.randint(11111,99999))}.exe'
+    open(path, 'wb').write(requests.get(a).content)
